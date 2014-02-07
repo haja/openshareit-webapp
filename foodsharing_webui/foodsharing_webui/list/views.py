00977ff51e61d0ad5d46b06820b1952fab093896
haja <h.jagenteufel@gmail.com>
2014-02-07 17:52:45 +0100

    display basic list of baskets

--- foodsharing_webui/foodsharing_webui/list/views.py
+++ foodsharing_webui/foodsharing_webui/list/views.py
@@ -1,4 +1,15 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from foodsharing_webui.list.mappings import Basket
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the list index.")
+    #TODO do RESTcall here
+
+    baskets = [Basket('title1', 'descr1'), Basket('title2', 'descr2')]
+
+    template = loader.get_template('foodsharing_webui/list/index.html')
+    context = RequestContext(request, {
+        'baskets': baskets,
+        })
+    return HttpResponse(template.render(context))
