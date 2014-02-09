3deac3b23896f1f5769981e883c1f6bb4aa22647
haja <h.jagenteufel@gmail.com>
2014-02-09 20:21:17 +0100

    more test rest-services

--- js-impl/hello.js
+++ js-impl/hello.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
         var jqxhr = $.get(
-            /*"http://api.ionic.at:8000/baskets/1/" */
-            "http://localhost:8000/json"
+            "http://api.ionic.at:8000/baskets/1/"
+            /*"http://localhost:8000/json" */
             /*url: "http://api.ionic.at/api/" */
-            /*url: "http://rest-service.guides.spring.io/greeting" */
+            /*"http://rest-service.guides.spring.io/greeting" */
                 /*"http://www.thomas-bayer.com/sqlrest/CUSTOMER/18/" */
             )
         .done(function(data) { $('.debug').append("success" + JSON.stringify(data)); })
