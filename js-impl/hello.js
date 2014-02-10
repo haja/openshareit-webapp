b9510d7de4ee4ff0ee2cfd9eca4fed421f85f056
haja <h.jagenteufel@gmail.com>
2014-02-10 14:25:08 +0100

    hello.js: log to console, test new API

--- js-impl/hello.js
+++ js-impl/hello.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
-        var jqxhr = $.get(
-            "http://api.ionic.at:8000/baskets/1/"
-            /*"http://localhost:8000/json" */
-            /*url: "http://api.ionic.at/api/" */
-            /*"http://rest-service.guides.spring.io/greeting" */
-                /*"http://www.thomas-bayer.com/sqlrest/CUSTOMER/18/" */
-            )
-        .done(function(data) { $('.debug').append("success" + JSON.stringify(data)); })
-        .fail(function(a, b, c) { $('.debug').append(JSON.stringify(a) + ";<br />\n" + JSON.stringify(b) + ";<br />\n"  + JSON.stringify(c)); })
+    var jqxhr = $.get(
+        "http://api.ionic.at/"
+        /*"http://localhost:8000/json" */
+        /*url: "http://api.ionic.at/api/" */
+        /*"http://rest-service.guides.spring.io/greeting" */
+        /*"http://www.thomas-bayer.com/sqlrest/CUSTOMER/18/" */
+        )
+        .done(function(data) {
+            window.console&&console.log(data);
+        })
+        .fail(function(a, b, c) {
+            window.console&&console.log(a);
+            window.console&&console.log(b);
+            window.console&&console.log(c);
+        });
 });
