de7ea772dc2292847544b6a98daaa5468fcc472a
haja <h.jagenteufel@gmail.com>
2014-02-13 12:44:43 +0100

    add api dummy data for abholdatum

--- js-impl/knockout/index-model-viewmodel.js
+++ js-impl/knockout/index-model-viewmodel.js
@@ -61,6 +61,7 @@ function ItemsViewModel() {
     self.queryTypes = ko.observableArray([
             new QueryType('Nähe', 'items_near', self.items)
             , new QueryType('Aktualität', 'items_fresh', self.items)
+            , new QueryType('Abholdatum', 'items_pick_up', self.items)
             ]);
 
     self.queryTypes()[0].query();
