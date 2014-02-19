81d737f4cdb6a0942ac4e992aa0d3f56c1cd3eb6
haja <h.jagenteufel@gmail.com>
2014-02-19 16:26:58 +0100

    my-items: parse/show date correctly

--- app/models/Profile.js
+++ app/models/Profile.js
@@ -5,7 +5,26 @@ define(['knockout'], function(ko) {
         self.firstName = data.firstName;
         self.lastName = data.lastName;
         self.email = data.email;
-        self.dateJoined = data.dateJoined;
+        self.dateJoined = function() {
+            // date format: yyyy-mm-ddThh:mm:ssZ
+            var parts = data.dateJoined.split('-');
+            var timeParts = parts[2]
+            .slice(1) // ignore 'T'
+            .split(':');
+            return new Date(parts[0]
+                , parts[1] - 1 // months are 0-based
+                , parts[2].slice(0,2
+                , timeParts[0]
+                , timeParts[1]
+                , timeParts[2].slice(0,2)
+                ));
+        }();
+        self.getDateJoinedAsString = function() {
+            window.console && console.log(self.dateJoined);
+            var d = self.dateJoined;
+            return [d.getDate(), d.getMonth() + 1, d.getFullYear()].join('.');
+        };
+
     };
     return Ctor;
 });
