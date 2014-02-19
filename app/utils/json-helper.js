fbb8fe0f37e76db44f1921c7f062e3438cc9ce81
haja <h.jagenteufel@gmail.com>
2014-02-19 15:11:27 +0100

    utils: refactor json-helper

--- app/utils/json-helper.js
+++ app/utils/json-helper.js
@@ -11,21 +11,27 @@ function(
     , Address
 ) {
     var obj = {};
-    var getWithCtor = function(Ctor, url, resultProperty, afterDoneHook) {
+    var getWithCtor = function(dataMapper, Ctor, url, resultProperty, afterDoneHook) {
         $.getJSON(
             url
         )
         .done(function(data) {
             window.console&&console.log(data);
-            resultProperty($.map(data, function(dataItem) { return new Ctor(dataItem) }));
+            resultProperty(dataMapper(data, Ctor));
             afterDoneHook && afterDoneHook();
         })
         .fail(function(a, b, c) { alert("fail");
         });
     };
+    var arrayMapper = function(data, Ctor) {
+        return _.map(data, function(dataItem) { return new Ctor(dataItem) })
+    };
+    var objMapper = function(data, Ctor) {
+        return new Ctor(data);
+    };
 
-    obj.getItems = _.partial(getWithCtor, Item);
-    obj.getAddresses = _.partial(getWithCtor, Address);
+    obj.getItems = _.partial(getWithCtor, arrayMapper, Item);
+    obj.getAddresses = _.partial(getWithCtor, arrayMapper, Address);
 
     return obj;
 });
