32415f80bec1f62e9899b086ce596f92a4be5096
haja <h.jagenteufel@gmail.com>
2014-03-11 17:26:06 +0100

    my-items: show request message, update api-mocks

--- app/models/Request.js
+++ app/models/Request.js
@@ -1,15 +1,18 @@
 define([
     'knockout'
+    , 'underscore'
     , 'models/User'
 ]
 , function(
     ko
+    , _
     , User
 ) {
     var Ctor = function Request(req) {
         var self = this;
         self.id = req.id;
         self.from = new User(req.from);
+        self.messages = req.messages;
     };
 
     return Ctor;
