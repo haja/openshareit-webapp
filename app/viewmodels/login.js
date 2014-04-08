984b2a3315c409c43fc3a043e18bc458e58ab7b1
haja <h.jagenteufel@gmail.com>
2014-04-08 19:20:49 +0200

    login: call api, login on succes, display error on fail

--- app/viewmodels/login.js
+++ app/viewmodels/login.js
@@ -1,19 +1,32 @@
 define([
-    'models/settings'
+    'knockout'
+    ,'models/settings'
+    , 'jquery'
 ],
 function(
-    settings
+    ko
+    , settings
+    , $
 ) {
     var ViewModel = function() {
         var self = this;
+
+        self.loginError = ko.observable(false);
 
         self.doLogin = function(formElement) {
             var email = formElement.elements.email.value;
             var pw = formElement.elements.password.value;
             window.console && console.log("email", email);
 
-            settings.token('testToken');
-            window.console && console.log("settings:", settings);
+            $.post('http://api.ionic.at/login/', 'username=' + email + '&password=' + pw, function(data) {
+                settings.token(data);
+                window.console && console.log("settings:", settings);
+                self.loginError(false);
+                // TODO trigger navigation
+            })
+            .fail(function() {
+                self.loginError(true);
+            });
         };
     };
 
