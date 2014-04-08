aa011a8e495b67ebc3a6c00312cf6661a6d4b85f
haja <h.jagenteufel@gmail.com>
2014-04-08 20:17:49 +0200

    login: add spinner while loading

--- app/viewmodels/login.js
+++ app/viewmodels/login.js
@@ -12,20 +12,26 @@ function(
         var self = this;
 
         self.loginError = ko.observable(false);
+        self.isLoading = ko.observable(false);
 
         self.doLogin = function(formElement) {
             var email = formElement.elements.email.value;
             var pw = formElement.elements.password.value;
             window.console && console.log("email", email);
 
+            self.isLoading(true);
+
             $.post('http://api.ionic.at/login/', 'username=' + email + '&password=' + pw, function(data) {
                 settings.token(data);
                 window.console && console.log("settings:", settings);
                 self.loginError(false);
+                self.isLoading(false);
+
                 // TODO trigger navigation
             })
             .fail(function() {
                 self.loginError(true);
+                self.isLoading(false);
             });
         };
     };
