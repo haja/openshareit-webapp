bd1ef7983cc5b173697b04434821a8571684a576
haja <h.jagenteufel@gmail.com>
2014-04-09 17:43:45 +0200

    my-items: use params to mark active request WIP

--- app/viewmodels/my-items.js
+++ app/viewmodels/my-items.js
@@ -26,10 +26,27 @@ function(
         var api_url = "../../api/"
         self.queryTypes = ko.observableArray([
             new QueryType('Meine Artikel', 'items_my',
-            function(items) {
-                self.items(items);
-                jsonHelper.getRequestsForItems(api_url + "request/", self.items)
-            }
+                function(items) {
+                    self.items(items);
+                    jsonHelper.getRequestsForItems(api_url + "request/", self.items,
+                        function() {
+                            window.console && console.log("my-items: queryChanged: got requests!");
+                            if(self.requestToActivate) {
+                                window.console && console.log("my-items: queryChanged: got item & request:", self.requestToActivate);
+                                var item, request;
+                                item = _.find(self.items(), function(it) {
+                                    return it.id === self.requestToActivate.itemId;
+                                });
+                                if(item) {
+                                    request = _.find(item.requests(), function(req) {
+                                        return req.id === self.requestToActivate.requestId;
+                                    });
+                                    request.active(true);
+                                    self.requestToActivate = false;
+                                }
+                            }
+                        });
+                }
             )
             , new QueryType('Angefragte Artikel', 'items_queried', self.items)
             , new QueryType('Abgeholte Artikel', 'items_picked_up', self.items)
@@ -42,6 +59,16 @@ function(
 
         self.actions = {
             showUserDialog: self.showUserDialog
+        };
+
+        self.activate = function(params) {
+            if(params && params.item && params.request) {
+                window.console && console.log("my-items: activate: got item & request:", params);
+                self.requestToActivate = {
+                    itemId: params.item
+                    , requestId: params.request
+                };
+            }
         };
 
         // load holderjs images
